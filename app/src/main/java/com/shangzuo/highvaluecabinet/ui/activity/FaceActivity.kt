57565cd9cd62a5a1dbca4e94187f323ab93062a5
YuLiang11 <1336739694@qq.com>
2023-12-08 10:38:21 +0800

    1.2.8

--- app/src/main/java/com/shangzuo/highvaluecabinet/ui/activity/FaceActivity.kt
+++ app/src/main/java/com/shangzuo/highvaluecabinet/ui/activity/FaceActivity.kt
@@ -195,7 +195,7 @@ class FaceActivity : BaseActivity<RecognizeViewModel, ActivityFaceBinding>(),
                     val layoutParams: ViewGroup.LayoutParams? = adjustPreviewViewSize(
                         mBind.dualCameraTexturePreviewRgb,
                         mBind.dualCameraTexturePreviewRgb, mBind.dualCameraFaceRectView,
-                        previewSizeRgb, displayOrientation, 1.5f
+                        previewSizeRgb, displayOrientation, 3.0f
                     )
                     if (layoutParams != null) {
                         rgbFaceRectTransformer = FaceRectTransformer(
